520712b5abda4cde7d5a7eece1960f670925b31a
sbb3391 <sbb33391@gmail.com>
2021-06-03 23:18:39 -0400

    populating new task form via javascript

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,8 +1,26 @@
-const filterButton = document.querySelector(".new-task")
+const newTaskButton = document.querySelector(".new-task")
+const taskTypes = {
+  "Customer Service": [
+    "Toner", "Billing", "Form Filling"
+  ],
+  "Orders": [
+    "Order Entry", "Order Fix"
+  ],
+  "Prospecting": [
+    "Follow Up", "Call", "Email"
+  ],
+  "Meeting": [
+    "Account Review", "First In", "In House Demo", "Proposal"
+  ],
+  "Current Customer": [
+    "Follow Up", "Call", "Email"
+  ]
+}
 
-filterButton.addEventListener("click", function() {
+newTaskButton.addEventListener("click", function() {
   showGreyBackground();
   showTaskForm();
+  populateTaskForm();
 })
 
 function showGreyBackground() {
@@ -18,6 +36,7 @@ function showTaskForm() {
   const formBackground = document.querySelector("div#form-background")
   const taskFormTemplate = document.querySelector("template#new-task-form");
   const taskForm = taskFormTemplate.content.firstElementChild.cloneNode(true);
+  taskForm.id = "task-form"
   const mainPage = document.querySelector("div#main-page");
 
   grayBackground.appendChild(taskForm);
@@ -32,4 +51,62 @@ function showTaskForm() {
       mainPage.classList.remove("filter", "blur-md");
     }
   })
+}
+
+function populateTaskForm() {
+  const taskForm = document.querySelector("div#task-form");
+  const startDate = taskForm.querySelector("#task-start-date");
+  const dueDate = taskForm.querySelector("#task-due-date");
+  const taskType = taskForm.querySelector("#task-type");
+  const taskSubtype = taskForm.querySelector("#task-sub-type");
+  let options = { year: "numeric", month: "2-digit", day: "2-digit"}
+
+  startDate.value = stringDate();
+  dueDate.value = stringDate();
+  getTaskTypes(taskTypes, taskType);
+  taskType.addEventListener("change", function() {
+    getSubtaskTypes(event, taskSubtype);
+  });
+}
+
+function stringDate() {
+  date = new Date();
+  y = date.getFullYear();
+  m = date.getMonth();
+  d = date.getDate();
+
+  dateString = y + "-" + (m <=9 ? "0" + (m + 1) : (m + 1)) + "-" + (d <=9 ? "0" + d : d);
+  return dateString;
+}
+
+function getTaskTypes(obj, ele) {
+  const keys = Object.keys(obj);
+  let option;
+
+  for (const key of keys) {
+    option = document.createElement("option");
+    option.setAttribute("value", key)
+    option.appendChild(document.createTextNode(key))
+    ele.appendChild(option);
+  }
+}
+
+function getSubtaskTypes(e, ele) {
+  const target = e.target;
+  const subTypeValues = taskTypes[target.value]
+  let option;
+
+  // remove all subtype options for select box
+  while (ele.lastElementChild) {
+    ele.lastElementChild.remove();
+  }
+
+  for (const value of subTypeValues) {
+    option = document.createElement("option");
+    option.setAttribute("value", value);
+    option.appendChild(document.createTextNode(value));
+    ele.appendChild(option);
+  }
+
+  ele.removeAttribute("disabled");
 }
